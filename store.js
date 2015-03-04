b7b864d23d6846ef3f94bafa333047408fce5d96
Dex Fitch <fitch.dexter@gmail.com>
2015-03-03 19:51:00 -0800

    Add minor styling.

--- store.js
+++ store.js
@@ -4,47 +4,55 @@ var quips = [
     body: 'All we have to decide is what to do with the time that is given us.',
     date: 'Wed Mar 03 2015 00:08:48 GMT-0800 (PST)',
     replies: [],
+    hashtag: "#TheFellowshipOfTheRing"
   },
   {
     id: '2',
     body: 'Would the world ever have been made if its maker had been afraid of making trouble? Making life means making trouble.',
     date: 'Wed Mar 03 2015 03:05:11 GMT-0800 (PST)',
-    replies: []
+    replies: [],
+    hashtag: "#Pygmalion"
   },
   {
     id: '3',
     body: 'Men\'s lives are as thoroughly blended with each other as the air they breathe: evil spreads as necessarily as disease.',
     date: 'Wed Mar 03 2015 05:08:17 GMT-0800 (PST)',
-    replies: []
+    replies: [],
+    hashtag: "#AdamBede"
   },
   {
     id: '4',
     body: 'We learn from failure, not from success!',
     date: 'Wed Mar 03 2015 07:58:01 GMT-0800 (PST)',
-    replies: []
+    replies: [],
+    hashtag: "#Dracula"
   },
   {
     id: '5',
     body: '"Adventurer" -- he that goes out to meet whatever may come. Well, that is what we all do in the world one way or another.',
     date: 'Wed Mar 03 2015 11:14:36 GMT-0800 (PST)',
-    replies: []
+    replies: [],
+    hashtag: "#AllanQuatermain"
   },
   {
     id: '6',
     body: '"Never," said my aunt, "be mean in anything; never be false; never be cruel. Avoid those three vices, and I can always be hopeful of you."',
     date: 'Wed Mar 03 2015 11:45:25 GMT-0800 (PST)',
-    replies: []
+    replies: [],
+    hashtag: "#DavidCopperfield"
   },
   {
     id: '7',
     body: 'Don\'t mind anything any one tells you about any one else. Judge everyone and everything for yourself.',
     date: 'Wed Mar 03 2015 11:45:59 GMT-0800 (PST)',
-    replies: []
+    replies: [],
+    hashtag: "#ThePortraitOfALady"
   },
   {
     id: '8',
     body: 'Reserving judgments is a matter of infinite hope.',
     date: 'Wed Mar 03 2015 11:57:23 GMT-0800 (PST)',
-    replies: []
+    replies: [],
+    hashtag: "#TheGreatGatsby"
   }
 ];
